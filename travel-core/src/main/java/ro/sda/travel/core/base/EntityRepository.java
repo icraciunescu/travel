c13aeb943591218952cee99998d7cce822503b0b
Ion Craciunescu <icraciunescu@hotmail.com>
2019-04-06 15:56:15 +0300

    entities client, host, property, booking, availability, payment and rating IV

--- travel-core/src/main/java/ro/sda/travel/core/base/EntityRepository.java
+++ travel-core/src/main/java/ro/sda/travel/core/base/EntityRepository.java
@@ -1,11 +1,11 @@
-package ro.sda.travel.core.base;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface EntityRepository<T extends BaseEntity> extends JpaRepository<T, Integer> {
-
-    T findById(int id);
-
-}
+//package ro.sda.travel.core.base;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.NoRepositoryBean;
+//
+//@NoRepositoryBean
+//public interface EntityRepository<T extends BaseEntity> extends JpaRepository<T, Integer> {
+//
+//    T findById(int id);
+//
+//}
